9326838bf2761a1c00f696ed8890475f45f964de
bitcamp <bitcamp@DESKTOP-42KHN9M>
2020-10-21 12:53:25 +0900

    회원 관련 컨트롤러 수정

--- finalTest/src/main/java/com/example/demo/Controller/MainController.java
+++ finalTest/src/main/java/com/example/demo/Controller/MainController.java
@@ -1,10 +1,5 @@
 package com.example.demo.Controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -12,12 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.DAO.HomeDAO;
 import com.example.demo.VO.CustomerVO;
-import com.example.demo.VO.InsertCustomerVO;
 
 @Controller
 public class MainController {
@@ -25,15 +17,6 @@ public class MainController {
 	@Autowired
 	private HomeDAO dao;
 
-	@RequestMapping("LoginPage.do")
-	public void loginPage() {}
-	
-	@RequestMapping("siteMap.do")
-	public void siteMap() {}
-	
-	@RequestMapping(value = "optOutCustomer.do", method = RequestMethod.GET)
-	public void optOutCustomer() {}
-	
 	@RequestMapping("/Home.do")
 	public String home(Model model, CustomerVO custVO, String loginOk, HttpServletRequest request) {
 		// 로그인 요청이 있는 경우
@@ -53,65 +36,5 @@ public class MainController {
 		model.addAttribute("HMakinglist", dao.getHomePost(20));
 		model.addAttribute("HMarketlist", dao.getHomePost(30));
 		return "Home";
-	}
-	
-	@RequestMapping("logout.do")
-	public String Logout(HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		session.removeAttribute("cust_no");
-		return "LoginPage";
-	}
-	
-	@RequestMapping(value = "insertCustomer.do", method = RequestMethod.GET)
-	public void insertCustomerPage() {}
-	
-	@RequestMapping(value = "insertCustomer.do", method = RequestMethod.POST)
-	public String insertCustomer(InsertCustomerVO vo, CustomerVO custVO, HttpServletRequest request, MultipartFile uploadfile) throws IllegalStateException, IOException {
-		String email = vo.getEmail1() + "@" + vo.getEmail2();
-		String addr = vo.getAddr2() + vo.getAddr3();
-		
-		//INTEREST
-		String[] values = request.getParameterValues("interest");
-		String interest = "";
-		for (int i = 0; i < values.length; i++) {
-			if (i == values.length-1) {
-				interest += values[i];
-			} else {
-				interest += (values[i] + ",");
-			}
-		}
-		
-		// file
-		String path = request.getServletContext().getRealPath("img");
-		if (!uploadfile.isEmpty()) {
-			String fname = uploadfile.getOriginalFilename();
-			uploadfile.transferTo(new File(path));
-			custVO.setFname(fname);
-		} else {
-			custVO.setFname("");
-		}
-		
-		custVO.setEmail(email);
-		custVO.setAddr(addr);
-		custVO.setInterest(interest);
-
-		
-		dao.insertCustomer(custVO);
-		return "redirect:Home.do";
-	}
-	
-	@RequestMapping(value = "optOutCustomerOk.do", method = RequestMethod.POST)
-	public String optOutCustomerOk(HttpServletRequest request) {
-		Map map = new HashMap();
-		HttpSession session = request.getSession();
-		if(request.getParameter("email") != null) {
-			String email = request.getParameter("email")+ "@" + request.getParameter("emailer");
-			String pw = request.getParameter("pw");
-			map.put("email", email);
-			map.put("pw", pw);
-			dao.OptOut(map);
-		}
-		session.removeAttribute("cust_no");
-		return "redirect:Home.do";
 	}
 }
